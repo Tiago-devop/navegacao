c4d36805a2aea26ad81070dd6570b3ac8fea263b
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-12-03 12:01:57 -0300

    Estrutura do projeto

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,4 @@
+import "./Content.css";
+import React from "react";
+
+const Content = (props) => <main className="Content">Componente Content</main>;
---
+++ src/views/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import React from "react";
+
+const App = (props) => (
+  <div className="App">
+    <h1>Componente App</h1>
+  </div>
+);
+
+export default App;
