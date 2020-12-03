ef8c1b8b95420a2cdeaed7f33ce626dbc605283e
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-12-03 12:54:30 -0300

    aplicando estilo

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import React from "react";
+import Menu from "../components/layout/Menu";
+import Content from "./../components/layout/Content";
 
 const App = (props) => (
   <div className="App">
-    <h1>Componente App</h1>
+    <Menu />
+    <Content />
   </div>
 );
 
