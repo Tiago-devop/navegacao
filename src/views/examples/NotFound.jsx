1989105507f4013264bda94943aec2618e1f19e1
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-12-08 14:16:47 -0300

    correção

--- src/views/examples/NotFound.jsx
+++ src/views/examples/NotFound.jsx
@@ -1,10 +1,10 @@
-import Reactimport { React } from 'react';
+import React from "react";
 
-const NotFound = props => (
-    <div className="NotFound">
-        <h1>404</h1>
-        <h2>Página não encontrada!</h2>
-    </div>
-)
+const NotFound = (props) => (
+  <div className="NotFound">
+    <h1>404</h1>
+    <h2>Página não encontrada!</h2>
+  </div>
+);
 
-export default NotFound
+export default NotFound;
