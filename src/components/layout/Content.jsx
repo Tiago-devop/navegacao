d3516ae7f372b7b6ed7df2daaa471e69e8500f13
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-12-08 14:02:59 -0300

    pagina não encontrada

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => (
   <main className="Content">
@@ -19,6 +20,10 @@ const Content = (props) => (
 
       <Route path="/">
         <Home />
+      </Route>
+
+      <Route path="*">
+        <NotFound />
       </Route>
     </Switch>
   </main>
